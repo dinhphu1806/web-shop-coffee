ac75683c6900cdcb209a60c2de6b76c7eee2a354
dinhphu1806 <dinhphu.le1806it@gmail.com>
2022-03-31 23:38:11 +0700

    lan 1

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+// const btns = document.querySelectorAll('#js-btn')
+// const modal = document.querySelector('.js-modal')
+// const modalClose = document.querySelector('.js-modal-close')
+// const modalContainer = document.querySelector('.js-modal-container')
+
+// function showBook(){
+//     modal.classList.add('open')
+// }
+// function hideBook(){
+//     modal.classList.remove('open')
+// }
+
+// for(const btn of btns){
+//     modal.addEventListener('click', showBook)
+// }
+//     modalClose.addEventListener('click', hideBook)
+
+//     modal.addEventListener('click', hideBook)
+
+// // click ra ngoài đóng
+// modalContainer.addEventListener('click', function(event){
+//     event.stopPropagation()
+// })
+const open = document.getElementById('js-btn');
+const jsModal = document.getElementById('js-modal');
+const close = document.getElementById('js-modal-close');
+
+open.addEventListener('click', () => {
+    jsModal.classList.add('show');
+});
+ close.addEventListener('click', () => {
+    jsModal.classList.remove('show');
+ })
