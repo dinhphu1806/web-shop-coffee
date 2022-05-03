4b6a5b36550edd2ccb18c75849bd707b3ca8eaf5
dinhphu1806 <dinhphu.le1806it@gmail.com>
2022-05-03 20:08:29 +0700

    commit initial

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-// js bar
+const menuBtn = document.querySelector('#menu-btn');
+const cartBtn = document.querySelector('#cart-btn');
+const searchBtn = document.querySelector('#search-btn');
+
 let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active'); 
     cartItem.classList.remove('active');
     searchForm.classList.remove('active');
@@ -9,22 +13,24 @@ document.querySelector('#menu-btn').onclick = () =>{
 // js cartItem
 let cartItem = document.querySelector('.cart-items-container');
 
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
+cartBtn.onclick = () =>{
     navbar.classList.remove('active');
+    cartItem.classList.toggle('active');
     searchForm.classList.remove('active');
 }
 //js search-form
 let searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active'); // bat seacrh-form
+searchBtn.onclick = () =>{
     navbar.classList.remove('active');// tat bnavbar
     cartItem.classList.remove('active'); // tat cartItem
+    searchForm.classList.toggle('active'); // bat seacrh-form
 }
-searchForm.addEventListener('click', function(event) {
-    event.stopPropagation();
-})
+
+
+// searchForm.addEventListener('click', function(event) {
+//     event.stopPropagation();
+// })
 // $(document).ready(function(){
 //     $(`.icon-bar`).clsick(function(){
 //         $(`ul`).toggleClass(`show`)
